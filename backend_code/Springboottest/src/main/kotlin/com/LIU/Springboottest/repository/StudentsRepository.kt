d5d89e9689045f0ac8d343240f18433eeddb4ee2
Xingwei Liu <57182515+hardworking-chris@users.noreply.github.com>
2022-06-22 15:41:13 +0900

    Delete backend_code/Springboottest directory

--- backend_code/Springboottest/src/main/kotlin/com/LIU/Springboottest/repository/StudentsRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.LIU.Springboottest.repository
-
-import com.LIU.Springboottest.entity.Student
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface StudentsRepository : JpaRepository<Student, Int>{
-
-}
